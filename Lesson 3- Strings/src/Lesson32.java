78f6f67c5a823fba833c26c527d3ca2c537a7080
tjordan2 <tjordan2@HS-D210-04.MOHS.local>
2019-02-26 12:13:07 -0500

    hi

---
+++ Lesson 3- Strings/src/Lesson32.java
@@ -0,0 +1,28 @@
+
+public class Lesson32 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("a\\b\\c\\\"d\"");
+		System.out.println("\\t is a tab, \\n is a new line," + "\" prints a \"quote\"");
+		System.out.println("line 1\n"+"\\line              two\\");
+		System.out.println("\\\\denotes a single line comment\n"+"\\*denotes a ultiline comment\\*");
+		System.out.println("\"\\\\\"\\\"\"");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Lesson 3- Strings/src/NameThatCelebrity.java
@@ -0,0 +1,31 @@
+
+public class NameThatCelebrity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s1 = "Allan Alda" ;
+		String s2 = "John Wayne";
+		String s3 = "Gregory Peck"; 
+		
+	
+		System.out.println(s1 + ">>>" + s1.substring(2,  s1.length()-3));
+		System.out.println(s2 + ">>>" + s2.substring(2,s2.length()-3 ));
+		System.out.println(s3 + ">>>" + s3.substring(2, s3.length()-3));
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
