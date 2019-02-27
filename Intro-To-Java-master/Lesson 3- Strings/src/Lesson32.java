5c0e63da47801ecc0f03fa52e5aedb0f6cb1ea57
kingt_000 <kingt_000@ASUSGTX965MPC>
2019-02-26 20:36:02 -0500

    At home 2

---
+++ Intro-To-Java-master/Lesson 3- Strings/src/Lesson32.java
@@ -0,0 +1,42 @@
+
+public class Lesson32 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Tyler Jordan 
+		//Numbers 1-5
+		
+		
+		
+		System.out.println("a\\b\\c\\\"d\"");
+		System.out.println("\\t is a tab, \\n is a new line," + "\" prints a \"quote\"");
+		System.out.println("line 1\n"+"\\line              two\\");
+		System.out.println("\\\\denotes a single line comment\n"+"\\*denotes a ultiline comment\\*");
+		System.out.println("\"\\\\\"\\\"\"\n" + "        \\\"\\\"\"\\\n" +"\\\"\"\"\"\\");
+		
+		//Number 6-13
+		
+		
+		String d = "Birthday";
+		System.out.println(d.toUpperCase());
+		
+		String a = "it's my party";
+		System.out.println(a.substring(0,4)+a.substring(4,7)+a.toUpperCase());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
