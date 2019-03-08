d85f722f977c9e741099b0815debab3d9ade377e
tjordan2 <tjordan2@HS-D210-04.MOHS.local>
2019-03-08 12:10:07 -0500

    3/8/19

---
+++ Lesson 5 -/src/CastingandConstant.java
@@ -0,0 +1,25 @@
+
+public class CastingandConstant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println((double)(95/9));
+		System.out.println(6.0/4);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
