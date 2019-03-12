687de91d7b95bd3a60a58aaaeb9e8b2a82814d47
tjordan2 <tjordan2@HS-D210-04.MOHS.local>
2019-03-12 14:16:59 -0400

    hi

--- Lesson 5 -/src/CastingandConstant.java
+++ Lesson 5 -/src/CastingandConstant.java
@@ -1,3 +1,6 @@
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+import org.omg.CORBA.SystemException;
 
 public class CastingandConstant {
 
@@ -8,15 +11,28 @@ public class CastingandConstant {
 		System.out.println(6.0/4);
 		
 		
+		double d = 13.25;
+		int i =12;
+		int j = (int)d/i;
+		System.out.println(j);
 		
 		
 		
+		int k = 7;
+		k*=5;
+		System.out.println(k);
 		
 		
+		double d1= 37.9;
+		double d2 = 1004.128;
+		int i1 = 12;
+		int i2 =18;
 		
-		
-		
-		
+		System.out.println(57.2*(i1/i2)+1);
+		System.out.println(57.2*(double)i1/i2+1);
+		System.out.println(15-i1*(d1*3)+4);
+		System.out.println(14-i1*(int)(d1*3)+4);
+		System.out.println();
 		
 		
 		
